86be84de27045b61bafe088cbfe64f9d96444b43
Administrador <administrador@iMac-de-Edson.local>
2024-01-24 17:14:43 -0600

    Subo cambios

---
+++ appamf/appamf/appamfApp.swift
@@ -0,0 +1,17 @@
+//
+//  appamfApp.swift
+//  appamf
+//
+//  Created by Administrador on 24/01/24.
+//
+
+import SwiftUI
+
+@main
+struct appamfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
